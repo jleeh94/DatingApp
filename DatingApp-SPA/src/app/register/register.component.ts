aabd288a98fa43d5720a1d0828da6abb40802d30
Jleeh94 <johnhart94@gmail.com>
2019-09-14 22:24:22 +0100

    End of Module 5 (Error Handling)

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -21,7 +21,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       console.log('Registered!');
     }, error => {
-      console.log('Failed to register');
+      console.log(error);
     });
   }
 
